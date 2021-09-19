7fb984824f67c69ddc053878cc059a180a681b92
cbru <charlesobrunson@gmail.com>
2021-09-19 16:48:30 -0500

    tileset template

---
+++ data/level/tileset/tile_basic_purple.tsx
@@ -0,0 +1,254 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tile_test" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="tile_basic_purple.png" width="128" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="shape" value="half"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="shape" value="halfvert"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="shape" value="halfvert-h"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="shape" value="half-v"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="shape" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="shape" value="slope-h"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="shape" value="shallow1"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="shape" value="shallow2"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="shape" value="shallow2-h"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="shape" value="shallow1-h"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="shape" value="slope-v"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="shape" value="slope-hv"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="shape" value="shallow1-v"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="shape" value="shallow2-v"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="shape" value="shallow2-hv"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="shape" value="shallow1-hv"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="shape" value="steep2"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="shape" value="steep2-h"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="shape" value="oneway-v"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="shape" value="oneway_wall-h"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="shape" value="steep1"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="shape" value="steep1-h"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="shape" value="oneway_wall"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="shape" value="oneway"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="shape" value="steep1-v"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="shape" value="steep1-hv"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="shape" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="shape" value="steep2-v"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="shape" value="steep2-hv"/>
+  </properties>
+ </tile>
+</tileset>
