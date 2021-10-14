36c438a588d25bdc7bf0d711830ece1cfa4c6b05
cbru <charlesobrunson@gmail.com>
2021-10-14 11:28:00 -0500

    fixed level

--- fastfall/include/fastfall/util/base64.hpp
+++ fastfall/include/fastfall/util/base64.hpp
@@ -6,6 +6,8 @@
 namespace ff {
 
 //std::string base64_encode(BYTE const* buf, unsigned int bufLen);
-std::vector<int8_t> base64_decode(std::string const&);
+std::vector<int8_t> base64_decode(std::string const& str);
+
+std::string base64_encode(int8_t const* buf, unsigned int len);
 
 }
--- fastfall/src/util/base64.cpp
+++ fastfall/src/util/base64.cpp
@@ -6,12 +6,57 @@ static const std::string base64_chars =
 "abcdefghijklmnopqrstuvwxyz"
 "0123456789+/";
 
-
 static inline bool is_base64(int8_t c) {
     return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
-std::vector<int8_t> ff::base64_decode(std::string const& encoded_string) {
+std::string base64_encode(std::vector<int8_t> const& data) {
+
+    int8_t const* buf = &data[0];
+    unsigned int len = data.size();
+
+    std::string ret;
+    int i = 0;
+    int j = 0;
+    int8_t char_array_3[3];
+    int8_t char_array_4[4];
+
+    while (len--) {
+        char_array_3[i++] = *(buf++);
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for (i = 0; (i < 4); i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for (j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (j = 0; (j < i + 1); j++)
+            ret += base64_chars[char_array_4[j]];
+
+        while ((i++ < 3))
+            ret += '=';
+    }
+
+    return ret;
+}
+
+std::vector<int8_t> ff::base64_decode(std::string const& encoded_string) 
+{
     int in_len = encoded_string.size();
     int i = 0;
     int j = 0;
