9dfe1a1893fe27034c253da8740d2c62b1702c46
cbru <charlesbrunson@github.com>
2022-09-24 06:46:44 -0500

    merge new_attempt to master

--- content/TestState.cpp
+++ content/TestState.cpp
@@ -87,6 +87,7 @@ void TestState::update(secs deltaTime) {
 					edit->select_layer(curr_layer);
 				}
 				layer = edit->get_tile_layer()->position;
+				//LOG_INFO("layer = {}", layer);
 			});
 		};
 
@@ -98,8 +99,13 @@ void TestState::update(secs deltaTime) {
 		layerOnKeyPressed(SDL_SCANCODE_KP_MINUS, -1);
 		layerOnKeyPressed(SDL_SCANCODE_KP_PLUS, 1);
 
-        onKeyPressed(SDL_SCANCODE_F1, [&]() { to_save = true; });
-        onKeyPressed(SDL_SCANCODE_F2, [&]() { to_load = true; });
+        onKeyPressed(SDL_SCANCODE_F1, [&]() {
+            to_save = true;
+        });
+
+        onKeyPressed(SDL_SCANCODE_F2, [&]() {
+            to_load = true;
+        });
 
 		onKeyPressed(SDL_SCANCODE_C, [&]() {
 				const auto* tilelayer = edit->get_tile_layer();
@@ -211,27 +217,22 @@ void TestState::update(secs deltaTime) {
 
 }
 
-void TestState::predraw(float interp, bool updated)
-{
+void TestState::predraw(float interp, bool updated) {
+
     world->predraw(interp, updated);
 
     if (to_save) {
-        if (save_world) {
-            *save_world = *world;
-        }
-        else {
-            save_world = std::make_unique<World>(*world);
-        }
-        to_save = false;
         LOG_INFO("saved state");
+        save_world = std::make_unique<World>(*world);
+        to_save = false;
     }
     else if (to_load) {
+        LOG_INFO("loaded state");
         if (save_world) {
             *world = *save_world;
             debug_draw::clear();
         }
         to_load = false;
-        LOG_INFO("loaded state");
     }
 
 	viewPos = world->camera().getPosition(interp);
@@ -324,4 +325,5 @@ void TestState::draw(ff::RenderTarget& target, ff::RenderState state) const
 			}
 		}
 	}
+
 }
