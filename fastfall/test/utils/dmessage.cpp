9715846d3fd2748fe40913bff5bf62d951a88a80
cbru <charlesobrunson@gmail.com>
2022-09-26 09:36:56 -0500

    finishing (?) dmessage

--- fastfall/test/utils/dmessage.cpp
+++ fastfall/test/utils/dmessage.cpp
@@ -1,18 +1,35 @@
 #include "gtest/gtest.h"
 
+#include "fastfall/util/math.hpp"
 #include "fastfall/util/dmessage.hpp"
 
 using namespace ff;
 
+// declare valid parameter types ( up to 16 for now )
+using dvar = std::variant<
+    std::monostate, // <- std::monostate is required at index zero
+    bool,
+    int,
+    float,
+    Vec2i,
+    Vec2f
+>;
+
+struct ExampleAddtlArg {
+
+};
+
+using my_config = dconfig<dvar, ExampleAddtlArg>;
+
 // define some message formats: message name, return type, message parameters... (up to 4 for now)
-constexpr auto dGetPosition  = dformat<"getpos", dtype::Vec2i>{};
-constexpr auto dSetPosition  = dformat<"setpos", dtype::Nil, dtype::Vec2i>{};
-constexpr auto dHasHPBetween = dformat<"ishpbetween", dtype::Bool, dtype::Float, dtype::Float>{};
-constexpr auto dUnhandled    = dformat<"unhandled">{};
+constexpr auto dGetPosition  = my_config::dformat<"getpos",    Vec2i>{};
+constexpr auto dSetPosition  = my_config::dformat<"setpos",    std::monostate, Vec2i>{};
+constexpr auto dHasHPBetween = my_config::dformat<"hpbetween", bool, float, float>{};
+constexpr auto dUnhandled    = my_config::dformat<"unhandled">{};
 
 // this mailbox responds correctly to messages
 struct Mailbox {
-    dresult message(dmessage msg) {
+    my_config::dresult message(ExampleAddtlArg hw, const my_config::dmessage& msg) {
         switch(msg)
         {
         case dGetPosition:
@@ -28,7 +45,7 @@ struct Mailbox {
             // OR
             return dHasHPBetween.apply(msg, [this](float min, float max) { return min <= health && health <= max; });
         }
-        return {};
+        return my_config::reject;
     }
 
     bool hasHPBetween(float min, float max) const {
@@ -41,7 +58,7 @@ struct Mailbox {
 
 // this mailbox responds incorrectly to messages
 struct Badbox {
-    dresult message(dmessage msg) {
+    my_config::dresult message(ExampleAddtlArg hw, const my_config::dmessage& msg) {
         switch(msg)
         {
         case dGetPosition:
@@ -55,8 +72,6 @@ struct Badbox {
 
 TEST(dmessage, wrap_unwrap)
 {
-    auto getpos_msg = dGetPosition.wrap();
-
     auto test_vec1 = Vec2i{12, 15};
 
     auto setpos_msg = dSetPosition.wrap(test_vec1);
@@ -75,18 +90,18 @@ TEST(dmessage, mailbox) {
     Mailbox box;
 
     Vec2i r1 = {100, 20};
-    Vec2i v1 = *dGetPosition.send(box);
+    Vec2i v1 = *dGetPosition.send(box, ExampleAddtlArg{});
     ASSERT_TRUE(v1 == Vec2i{});
 
-    dSetPosition.send(box, r1);
+    dSetPosition.send(box, ExampleAddtlArg{}, r1);
 
-    Vec2i v2 = *dGetPosition.send(box);
+    Vec2i v2 = *dGetPosition.send(box, ExampleAddtlArg{});
     ASSERT_TRUE(v2 == r1);
 
-    auto h = dUnhandled.send(box);
+    auto h = dUnhandled.send(box, ExampleAddtlArg{});
     ASSERT_FALSE(h);
 
-    bool hp = *dHasHPBetween.send(box, 20.f, 80.f);
+    bool hp = *dHasHPBetween.send(box, ExampleAddtlArg{}, 20.f, 80.f);
     ASSERT_TRUE(hp);
 }
 
@@ -94,10 +109,10 @@ TEST(dmessage, badbox) {
     Badbox box;
 
     Vec2i r1 = {100, 20};
-    ASSERT_ANY_THROW(dGetPosition.send(box));
-    ASSERT_ANY_THROW(dSetPosition.send(box, r1));
+    ASSERT_ANY_THROW(dGetPosition.send(box, ExampleAddtlArg{}));
+    ASSERT_ANY_THROW(dSetPosition.send(box, ExampleAddtlArg{}, r1));
 
-    auto h = dUnhandled.send(box);
+    auto h = dUnhandled.send(box, ExampleAddtlArg{});
     ASSERT_FALSE(h);
 }
 
