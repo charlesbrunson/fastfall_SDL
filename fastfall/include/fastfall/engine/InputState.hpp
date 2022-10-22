d7ae1df4dbc45169236ed7dd6fd1881726ae82a6
cbru <charlesbrunson@github.com>
2022-10-22 11:01:23 -0500

    cleaning up & prep for input recording

--- fastfall/include/fastfall/engine/InputState.hpp
+++ fastfall/include/fastfall/engine/InputState.hpp
@@ -19,13 +19,8 @@ public:
     InputState() = default;
     InputState(const std::vector<InputType>& listen_inputs);
 
-
     void update(secs deltaTime);
     bool push_event(SDL_Event e);
-
-    //bool is_pressed(InputType input, secs bufferWindow = 0.0) const;
-    //bool is_held(InputType input) const;
-    //bool confirm_press(InputType input);
 
     Input& operator[](InputType in) { return input_states.at(in); }
     const Input& operator[](InputType in) const { return input_states.at(in); }
@@ -36,18 +31,17 @@ public:
     Input* get(InputType in) { return input_states.contains(in) ? &at(in) : nullptr; }
     const Input* get(InputType in) const { return input_states.contains(in) ? &at(in) : nullptr; }
 
-    void listen(InputType input);
-    void unlisten(InputType input);
-    void listen_config(const std::vector<InputType>& listen_inputs);
+    bool is_listening(InputType in) const;
+    bool is_listening(std::optional<InputType> in) const;
 
-    bool is_listening(InputType in) const { return input_states.contains(in); }
-    bool is_listening(std::optional<InputType> in) const { return in && input_states.contains(*in); }
+    void set_tick(size_t tick);
+    size_t get_tick() const;
 
     static std::vector<InputType> config_gameplay;
 private:
     Input* get_state(InputType input);
 
-    void process_events(secs deltaTime);
+    void process_events();
     void process_axis(const InputConfig::GamepadInput* gamepad, Input* input, int16_t axis_pos, int16_t alt_axis_pos);
 
     struct InputEvent {
@@ -58,6 +52,7 @@ private:
 
     std::map<InputType, Input> input_states;
     std::vector<InputEvent> events;
+    size_t input_tick = 0;
 };
 
 
