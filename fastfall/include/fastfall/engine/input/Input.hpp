85d5cbc177f607499e55ad44cc288cf935af8273
cbru <charlesbrunson@github.com>
2022-10-22 10:16:36 -0500

    input magnitude velocity

--- fastfall/include/fastfall/engine/input/Input.hpp
+++ fastfall/include/fastfall/engine/input/Input.hpp
@@ -29,7 +29,6 @@ public:
 	bool is_confirmable()	const { return /* active && */ !confirmed; };
 
 	int num_activators()	const { return activeCounter; };
-
 	secs get_last_pressed() const { return lastPressed; }
 
 	const InputType type;
@@ -42,10 +41,20 @@ public:
 	Vec2i mouse_press_pos;
 	Vec2i mouse_release_pos;
 
-
     constexpr static uint8_t MAG_ZERO = 0x0;
     constexpr static uint8_t MAG_FULL = 0xFF;
-    uint8_t magnitude = MAG_ZERO;
+
+    void set_magnitude(uint8_t mag) { curr_magnitude = mag; }
+
+    uint8_t magnitude() { return curr_magnitude; }
+    int velocity() { return curr_velocity; }
+
+    /*
+    void update(secs deltaTime) {
+        velocity = (int)(((double)magnitude - (double)prev_magnitude) / deltaTime);
+        prev_magnitude = magnitude;
+    }
+    */
 
 private:
 
@@ -58,6 +67,10 @@ private:
 
 	secs lastPressed = DBL_MAX;
 	secs lastHoldDuration = 0.0;
+
+    uint8_t curr_magnitude = MAG_ZERO;
+    uint8_t prev_magnitude = MAG_ZERO;
+    int curr_velocity = 0;
 };
 
 }
