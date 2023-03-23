800ca3d35ba54cd97e37793bfe6ac4981aec436d
cbru <charlesbrunson@github.com>
2023-03-22 21:02:26 -0500

    dfas

--- fastfall/include/fastfall/game/imgui_component.hpp
+++ fastfall/include/fastfall/game/imgui_component.hpp
@@ -4,8 +4,7 @@
 
 namespace ff {
     class World;
+
     void imgui_component_ref(const World &w, const ComponentID& cmp);
-
     void imgui_component(World&, ComponentID id);
-
 }
--- fastfall/include/fastfall/util/Line.hpp
+++ fastfall/include/fastfall/util/Line.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-//#include <SFML/System.hpp>
-
 #include <stdexcept>
 
 #include "fastfall/util/Vec2.hpp"
