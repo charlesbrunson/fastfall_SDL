3a490080802bc8d17d154b3f0ea2e9393ad6c041
cbru <charlesbrunson@github.com>
2024-06-04 10:55:14 -0500

    moving stuff around

--- fastfall/include/fastfall/fastfall.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <filesystem>
-
-namespace ff {
-    bool Init();
-    bool Load_Resources(std::filesystem::path root);
-    void Quit();
-}
--- main.cpp
+++ main.cpp
@@ -1,46 +1,5 @@
-
-#include "fastfall/engine/Engine.hpp"
-#include "fastfall/render/target/Window.hpp"
-#include "fastfall/fastfall.hpp"
-
-#include "content/types.hpp"
-#include "content/TestState.hpp"
-
+#include <spdlog/spdlog.h>
 
 int main(int argc, char* argv[]) {
-    register_types();
-
-    // initialize subsystems
-    if (ff::Init())
-    {
-        // create an opengl context
-        auto* window = new ff::Window{ true };
-        ff::Engine* engine = nullptr;
-
-        // load resources from file
-        if (ff::Load_Resources( "data/" ))
-        {
-            // create the engine
-            engine = new ff::Engine{ window };
-
-            // give it something to run
-            engine->make_runnable<TestState>();
-
-            // run it
-            engine->run();
-        }
-#if not defined(__EMSCRIPTEN__)
-
-
-        if (engine)
-            delete engine;
-
-        ff::Quit();
-
-        delete window;
-
-#endif
-    }
-
-	return EXIT_SUCCESS;
+   spdlog::info("huh");
 }
