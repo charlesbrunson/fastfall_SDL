77e342bc6616f12cbf25d0fdcbd4357317983500
cbru <charlesobrunson@gmail.com>
2024-06-30 12:01:54 -0500

    drawing a spinning triangle

--- main.cpp
+++ main.cpp
@@ -12,33 +12,38 @@
 constexpr std::string_view vert_src = R"(
 #version 330 core
 
-layout (location = 0) in vec3 t_pos;
-layout (location = 1) in vec4 t_color;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aColor;
 
-out vec4 m_color;
+uniform mat4 uView;
+uniform mat4 uModel;
+uniform mat4 uProj;
+
+out vec4 vColor;
 
 void main()
 {
-  gl_Position = vec4(t_pos, 1.0f);
-  m_color = t_color;
+  gl_Position = uProj * uView * uModel * vec4(aPos, 1.0f);
+  vColor = aColor;
 }
 )";
 
 constexpr std::string_view frag_src = R"(
 #version 330 core
 
-in vec4 m_color;
-out vec4 outputColor;
+in vec4 vColor;
+out vec4 fColor;
 
 void main()
 {
-  outputColor = m_color;
+  fColor = vColor;
 }
 )";
 
 struct vertex {
     ff::vec3 pos;
     ff::color col;
+
     using attributes = ff::v_attr_list<
         ff::v_attr<3, float>,
         ff::v_attr<4, ff::u8, true>
@@ -46,17 +51,17 @@ struct vertex {
 };
 
 vertex vertices[] = {
-    { { -0.5f, -0.5f, 1.0f }, ff::color::red }, // left
-    { {  0.5f, -0.5f, 1.0f }, ff::color::red }, // right
-    { {  0.0f,  0.5f, 1.0f }, ff::color::red }  // top
+    { { -0.5f, -0.5f, 0.0f }, ff::color::red }, // left
+    { {  0.5f, -0.5f, 0.0f }, ff::color::green }, // right
+    { {  0.0f,  0.5f, 0.0f }, ff::color::blue }  // top
 };
 
 class test_app : public ff::application {
 public:
     test_app()
     : ff::application{ "test_app" }
-    , varr{ vbuf }
     , vbuf{ vertices }
+    , varr{ vbuf }
     {
         test_shader = *ff::shader_factory{}
             .add_vertex("vert.glsl", vert_src)
@@ -67,16 +72,23 @@ public:
     };
 
     void update(ff::seconds deltaTime) override {
-
+        time_prev = time_next;
+        time_next += deltaTime;
     };
 
     void predraw(ff::tick_info predraw_state, ff::render_info win_info) override {
-
+        time = std::lerp(time_prev, time_next, predraw_state.interp);
     };
 
     void draw(const ff::render_target& t_target) override {
 
+        glViewport(0, 0, t_target.size().x, t_target.size().y);
+
         test_shader.bind();
+        test_shader.set("uView",  glm::mat4{ 1 });
+        test_shader.set("uModel", glm::rotate(glm::mat4{ 1 }, (float)time, glm::vec3(0.f, 1.f, 0.f)));
+        test_shader.set("uProj",  glm::ortho(-1.f, 1.f, -1.f, 1.f));
+
         varr.bind();
         glDrawArrays(GL_TRIANGLES, 0, 3);
     };
@@ -87,6 +99,11 @@ public:
 
 private:
     bool show_demo = true;
+
+    ff::seconds time_prev = 0.f;
+    ff::seconds time_next = 0.f;
+    ff::seconds time = 0.f;
+
 
     ff::shader test_shader;
     ff::vertex_buffer vbuf;
